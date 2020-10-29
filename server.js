79987dd4970a62531bac63959b6f2f415e2a0abd
student <student@eda-bc-03.localdomain>
2020-10-29 17:52:34 +1300

    server set-up

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+//const route = require('./routes')
+
+const server = express()
+
+// Middleware
+
+server.engine('hbs', hbs({
+    defaultLayout: 'main',
+    extname: 'hbs'
+  }))
+  
+  server.set('view engine', 'hbs')
+  server.use(express.static('public'))
+  server.use(express.urlencoded({extended: false}))
+
+
+
+
+
+
+
+  
+  module.exports = server
